6aafe6cb75767ffc48cbec9db32df29109dd775d
Mootbing <50122069+Mootbing@users.noreply.github.com>
2021-10-11 16:59:14 -0400

    Add support to direct translation too

--- SRC/Main.py
+++ SRC/Main.py
@@ -5,7 +5,8 @@ import math as albreto
 
 from bs4 import BeautifulSoup
 
-Link = "https://www.spanishdict.com/conjugate/"
+LinkConj = "https://www.spanishdict.com/conjugate/"
+LinkDef = "https://www.spanishdict.com/translate/"
 Words = []
 
 with open(os.path.dirname(os.path.realpath(__file__)) + "/Output.txt", "w+") as File:
@@ -15,8 +16,25 @@ with open(os.path.dirname(os.path.realpath(__file__)) + "/Input.txt", "r") as Fi
     Words = [Word.strip("\n") for Word in File.readlines()]
 
 for Word in Words:
-    URL = requests.get(f"{Link}{Word.lower()}")
-    Soup = BeautifulSoup(URL.content, "html.parser")
+
+    LinkBase = LinkDef
+    if Word.startswith("v "):
+        LinkBase = LinkConj
+
+    URL = f"{LinkBase}{'%20'.join(Word.split(' ')[1:]).lower()}"
+    Word = "".join(Word.split(" ")[1:])
+
+    URLResult = requests.get(URL)
+    Soup = BeautifulSoup(URLResult.content, "html.parser")
+
+    print(f"{URL.lower()}")
+
+    #get good word from website with accent
+    CorrectedWord = Soup.find("h1", attrs={"class":"cXUN5-ST"})
+
+    if CorrectedWord is not None:
+        Word = re.sub("""(<.*">)|(</.*>)""", "", str(CorrectedWord))
+
     Conjugations = [[[] for _ in range(5)] for _ in range(6)]
 
     i = 0
@@ -36,4 +54,7 @@ for Word in Words:
     Defination = re.sub('''</.*>''', "", str(Defination))
 
     with open(os.path.dirname(os.path.realpath(__file__)) + "/Output.txt", "a", encoding="UTF-8") as File:
-        File.write(f"{Defination}, {Word} ({' '.join(Conjugations[0][1])})\n") #flip def and word so spanish is with spanish
+        
+        CondensedConj = f"({' '.join(Conjugations[0][1])})" if Conjugations[0][1] != [] else " "
+        
+        File.write(f"{Defination}, {Word} {CondensedConj}\n") #flip def and word so spanish is with spanish
